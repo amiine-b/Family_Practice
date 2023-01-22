f3a5192d1ab54a4d6c5abacc8b2560fb1f972f8b
Bassma <bassma.benaz@gmail.com>
2023-01-21 21:31:26 -0500

    Sammy

--- src/Class_4.java
+++ src/Class_4.java
@@ -4,6 +4,6 @@ public class Class_4 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I am going to work on my project.");
-
+        System.out.println("Sammy 2022");
     }
 }
