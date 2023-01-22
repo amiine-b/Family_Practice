c1e75ad0f83dad81070628a782e8915d91e67b80
abdellahbouziane <abouz77@gmail.com>
2023-01-21 22:10:23 -0500

    Conflict ticket_0013

--- src/Conflict.java
+++ src/Conflict.java
@@ -2,5 +2,10 @@ public class Conflict {
     public static void main(String[] args) {
         System.out.println("this amine");
         System.out.println("aamine");
+        System.out.println("Abdellah");
+
+
+        System.out.println("Lojain");
+
     }
 }
