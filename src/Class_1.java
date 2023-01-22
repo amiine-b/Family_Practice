4fce363144a90cf7bd198e4f8b25b47d24279c2c
iAmine <bergui.amine@gmail.com>
2023-01-21 21:21:55 -0500

    this is amine bergui commit

--- src/Class_1.java
+++ src/Class_1.java
@@ -2,5 +2,6 @@ public class Class_1 {
     public static void main(String[] args) {
         // This is amine
         System.out.println("Hello World");
+        System.out.println("This Wednesday. I am going to work on login functionally ");
     }
 }
